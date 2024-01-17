74c46e7b7128b75c853d180dbe6d5ab6dd260847
uppie51 <152970739+uppie51@users.noreply.github.com>
2024-01-17 13:01:43 +0100

    Ex.Phynton

---
+++ ex1.py
@@ -0,0 +1,124 @@
+'''INDIVIDURARE TRE TASK DI ALGORITMI 
+# Primo Algoritmo Fare la spesa
+
+ Scrivi su un foglio la lista 
+Prendi una lista della spesa.
+Vai al supermercato più vicino.
+Prendi il carrello
+Vai all'area scelta nella lista quella che vuoi tu
+Prendi   la giusta quantita di quello che ti serve 
+Metti gli articoli
+Vai alla cassa.
+Paga 
+
+
+
+#Secondo Algoritmo  Riempire il serbatoio
+
+Verifica i soldi 
+Aprire all intenro della macchina schiacciare il pulsante
+Apri tappo del serbatoio dell'auto.
+paga 
+Scegli il tipo di carburante corretto per il veicolo.
+Alza la  pompa di benzina/diesel.
+Inizia a rifornire il serbatoio
+Controlla il livello di carburante  desiderato e ferma il rifornimento quando raggiunge la capacità desiderata.
+Chiudi il coperchio del serbatoio e quello della pompa di benzina.
+
+
+
+
+
+
+#Terzo Algoritmo
+
+ Algoritmo di Studiare un concetto
+
+Scegli cosa stiare (argomento che ti interessi/ti piaccia)
+Prendi  quello che ti serve per studiare quella determinata cosa esempio dispense libri etc
+Organizzatti da dove iniziare (es  primo secondo terzo)
+Inizia  dalle basi per poi avanzare nello studio
+Prendi appunti 
+Fai esercizi pratici per applicare i concetti appresi.
+ripeti  
+Verificazione dei concetti
+
+'''
+#abbiamo 25 studenti; memorizzare questo dato in una variabile. Utilizzeremo: • l'operatore di assegnazione =
+num_studenti= 25
+
+#Abbiamo 25 studenti; memorizzare questo dato in una variabile  e stamparla a video.
+num_studenti= 25
+print(num_studenti)
+
+#Abbiamo 25 studenti; memorizzare questo dato in una variabile, arrivano altri 3 studenti; memorizzare questo dato  in un altra variabile
+num_studenti= 25
+new_student= 3
+
+#abbiamo 25 studenti; memorizzare questo dato in una variabile. Arrivano altri 3 studenti; memorizzare questo dato in un altra variabile.Creare  un altra variabile che conterra la sum delle prime due, poi stampa a video
+num_studenti=25
+print (num_studenti)
+new_student=3
+Sum_Studenti=num_studenti + new_student
+print(Sum_Studenti)
+
+#Creare una variabile che contiene la stringa 'EPICODE', quindi stamparlo a video.
+
+corso="EPICODE"
+print(corso)
+
+
+#Creare a mano una lista che contenga i numeri da 0 a 5, memorizzarla in una variabile e infine stamparla a video.
+numeri_List=[0,1,2,3,4]
+print(numeri_List)
+
+#AAbbiamo la stringa: nome_scuola = "Epicode" Stampare l'iniziale.
+nome_scuola = "Epicode"
+Primalettera = nome_scuola[0]
+print(Primalettera)
+
+#Esercizio Abbiamo la stringa: nome_scuola = "Epicode" Stampare le prime tre lettere.
+
+nome_scuola = "Epicode"
+Primitre= nome_scuola[0:3]
+print(Primitre)
+
+
+#Abbiamo la variabile: x = 10 Incrementarla di 2 e poi moltiplicarla per 3 Usare due metodi diversi (ad esempio, uno utilizzando gli operatori di assegnazione, e uno senza)
+
+x = 10
+y = (x + 2) * 3
+print(y)
+
+  #Esercizio Scriviamo un programma che chiede in input all'utente una stringa e visualizza i primi 3 caratteri, seguiti da 3 punti di sospensione e quindi gli ultimi 3 caratteri. Esempio: • Input: computer • Output: com...ter Utilizzeremo: • La funzione input()
+Utente_inse= input("inserisci un testo")
+
+Output=Utente_inse[0:4]+"..."+Utente_inse[-4: ]
+print(Output)
+
+
+# verifcare  per ogniuna delle seguenti strimghe se il numero di carattere  e compreso trea 5 e 8  windows excel power point word.
+
+Verifica1= "Windows"
+Verifica2="power point"
+Verifica3="excel"
+Verifica4="word"
+
+Verifica1= 5 <= len("Windows")<=8
+Verifica2= 5<len("Power point")<= 8
+Verifica3=5<len("excel")<= 8
+Verifica4 =5<=len("word") <= 8
+
+print("Verifica1:", Verifica1)
+print("Verifica2:", Verifica2)
+print("Verifica3:", Verifica3)
+print("Verifica4:", Verifica4)
+
+#Inventare un programma che prende in input degli  elementi e restituiscono in ouput una  loro combinazione (ad esempio prendendo  in imput due numeri e restituendo le loro somma)
+
+utente1=  float(input("inserire un numero"))
+utente2= float(input("inserisci un altro numero"))
+sommaduenumeri=utente1+utente2
+print("La somma di entambi i numeri e :", sommaduenumeri)
+
+
